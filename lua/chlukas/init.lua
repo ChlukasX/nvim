7e511f32450e9f46a3d0dad302eccda7351342da
Chlukas <lukasnackmayr@proton.me>
2025-06-15 23:27:56 +0200

    Made colors actually init

--- lua/chlukas/init.lua
+++ lua/chlukas/init.lua
@@ -2,3 +2,5 @@ require("chlukas.set")
 require("chlukas.remap")
 
 require("chlukas.lazy_init")
+
+vim.cmd.colorscheme "onedark"
--- lua/chlukas/plugins/colors.lua
+++ lua/chlukas/plugins/colors.lua
@@ -1,8 +1,26 @@
-return{
-	{
-  "folke/tokyonight.nvim",
-  lazy = false,
-  opts = { style = "moon" },
-	}
+return {
+    {
+        'ribru17/bamboo.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require('bamboo').setup {
+                -- optional configuration here
+            }
+            require('bamboo').load()
+        end,
+    },
+    {
+        "navarasu/onedark.nvim",
+        priority = 1000,
+        enable = false,
+        config = function()
+            require('onedark').setup {
+                style = 'dark'
+            }
+            -- Enable theme
+            require('onedark').load()
+        end
+    },
 }
 
