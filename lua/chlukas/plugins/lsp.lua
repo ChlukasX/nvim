2fe7cdb0c0aedbd5fbd078b61352243ec91c1831
Chlukas <lukasnackmayr@proton.me>
2025-06-15 23:28:27 +0200

    lsp changes

--- lua/chlukas/plugins/lsp.lua
+++ lua/chlukas/plugins/lsp.lua
@@ -8,7 +8,11 @@ return {
     {
         "mason-org/mason-lspconfig.nvim",
         opts = {
-            ensure_installed = { "lua_ls" },
+            ensure_installed = { 
+                "lua_ls",
+                "gopls",
+                "tinymist"
+            },
         },
         dependencies = {
             { "mason-org/mason.nvim", opts = {} },
@@ -18,9 +22,18 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.tinymist.setup({})
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.gopls.setup({
+                capabilites = capabilities
+            })
+            lspconfig.tinymist.setup({
+                capabilites = capabilities
+            })
 
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
