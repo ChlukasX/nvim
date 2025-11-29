b20270778e892fc8d9d5628fc6a4fe1442142470
Chlukas <lukasnackmayr@proton.me>
2025-11-29 22:32:47 +0100

    devcontainer and fugitive

--- lua/chlukas/plugins/typst.lua
+++ lua/chlukas/plugins/typst.lua
@@ -1,6 +1,17 @@
 return {
-    'chomosuke/typst-preview.nvim',
-    lazy = false,
-    version = '1.*',
-    opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+    {
+        "chomosuke/typst-preview.nvim",
+        lazy = false,
+        version = "1.*",
+        opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+    },
+    {
+        "lervag/vimtex",
+        lazy = false, -- we don't want to lazy load VimTeX
+        -- tag = "v2.15", -- uncomment to pin to a specific release
+        init = function()
+            -- VimTeX configuration goes here, e.g.
+            vim.g.vimtex_view_method = "zathura"
+        end,
+    },
 }
