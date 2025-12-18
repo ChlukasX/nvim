a723015a371f64531aea8b8735d2de1e3e81f3ce
Chlukas <lukasnackmayr@proton.me>
2025-12-18 10:24:24 +0100

    ros, supermave, obsidian

--- lua/chlukas/plugins/obsidian.lua
+++ lua/chlukas/plugins/obsidian.lua
@@ -1,16 +1,8 @@
 return {
     "epwalsh/obsidian.nvim",
-    version = "*",  -- recommended, use latest release instead of latest commit
+    version = "*", -- recommended, use latest release instead of latest commit
     lazy = true,
     ft = "markdown",
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    --   -- refer to `:h file-pattern` for more examples
-    --   "BufReadPre path/to/my-vault/*.md",
-    --   "BufNewFile path/to/my-vault/*.md",
-    -- },
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
@@ -22,15 +14,52 @@ return {
             },
             {
                 name = "kasten",
-                path = "~/Documents/kasten/",
+                path = "~/Documents/kasten",
             },
         },
         templates = {
-            folder = "templates"
+            folder = "templates",
+            date_format = "%Y-%m-%d-%a",
+            time_format = "%H:%M",
         },
         ui = {
             enable = true,
-
         },
-    }
+        picker = {
+            name = "telescope.nvim",
+            note_mapping = {
+                new = "<C-x>",
+                insert_link = "<C-l>",
+            },
+            tag_mapping = {
+                tag_note = "<C-x>",
+                insert_tag = "<C-l>",
+            },
+        },
+        daily_notes = {
+            folder = "notes/dailies",
+            date_format = "%Y-%m-%d",
+            default_tags = { "daily-notes" },
+            template = nil,
+        },
+
+        mappings = {
+            ["<leader>ch"] = {
+                action = function()
+                    return require("obsidian").util.toggle_checkbox()
+                end,
+                opts = { buffer = true },
+            },
+            ["<leader>on"] = {
+                action = function ()
+                    return vim.keymap.set("n", "<leader>on", ":ObsidianNew<CR>")
+                end
+            },
+            ["<leader>ot"] = {
+                action = function ()
+                    return vim.keymap.set("n", "<leader>ot", ":ObsidianNewFromTemplate<CR>")
+                end
+            },
+        },
+    },
 }
